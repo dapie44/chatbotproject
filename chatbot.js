d7cd129bbf4ccbc2d2a726ff51bba40d92d7d40e
Anthony Sandoval <retrovirus546@gmail.com>
2016-09-23 13:54:35 -0700

    i tried

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,15 @@
-function Speak(){
-  $("#chat-area").prepend("I'm back!");
+var questions = {
+  "hello" : "howdy",
+  "how old are you?" : "I used to be 17",
+  "what is  your name?" : "I am Harambe"
 }
+function Speak(){
+ var ChatInput = $("#input").val();
+ var converting;
+ var storage = "";
+ converting = ChatInput.split('');
+for( i =0; i < converting.length; i++){
+   storage += letterConversion[converting[i]];
+ }
+ console.log( storage );
+ ChatInput = storage;
