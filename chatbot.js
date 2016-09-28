0ee0a1aefb4d2c62cbf92b718e0bffe934a23c5a
Anthony Sandoval <retrovirus546@gmail.com>
2016-09-28 14:04:05 -0700

    improving

--- chatbot.js
+++ chatbot.js
@@ -8,6 +8,6 @@ document.getElementById("chat-area").innerHTML += chatInput + ": "+ sayResponse 
  var questions = {
    "Hello" : "howdy",
    "How old are you?" : "I don't have an age",
-   "What is  your name?" : "I am Steve",
+   "What is  your name?" : "I am Chuck E Cheese",
    "What time is it?" : "Why should I tell you"
  }
